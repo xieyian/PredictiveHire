60097c3aa8097f6ffb89bf2aa206b75daaa0f094
Yian Xie A1702241 <a1702241@student.adelaide.edu.au>
2021-05-10 15:10:26 +0930

    add main page buttons

---
+++ interview/src/Signin.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Signin = () => {
+	return <div>Signin page</div>;
+};
+export default Signin;
---
+++ interview/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Header />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
