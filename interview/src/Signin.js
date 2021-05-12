dfd366d8db2402667a749da28dfd9a606384a56d
Yian Xie A1702241 <a1702241@student.adelaide.edu.au>
2021-05-12 12:23:45 +0930

    finish fack endpoint and applied for check sigin successful

--- interview/src/Signin.js
+++ interview/src/Signin.js
@@ -2,23 +2,35 @@ import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import creatHistory from 'history/createHashHistory';
-// 如果使用createBrowserHistory
-// import creatHistory from 'history/createBrowserHistory';
 import './Signin.scss';
 
 export default function Signin() {
 	const [ name, setName ] = useState('');
 	const [ password, setPassword ] = useState('');
+	var check = false;
 
 	function validateForm() {
 		return name.length > 0 && password.length > 0;
 	}
-	function checkPassword() {
-		if (password === 'YianWasHired') {
-			console.log('successful');
+
+	// function checkPassword() {
+	const axios = require('axios');
+	axios.get('http://localhost:3000/employees').then((resp) => {
+		resp.data.forEach((e) => {
+			if (password === e.password && name === e.name) {
+				check = true;
+			}
+		});
+	});
+
+	function output() {
+		if (check === true) {
+			alert('Log in successful');
+			var history = creatHistory();
+			history.push({ pathname: '/' });
 		} else {
+			alert('Wrong User name or Password');
 			setPassword('');
-			alert('Wrong Password!!!');
 		}
 	}
 	function cancel() {
@@ -31,35 +43,37 @@ export default function Signin() {
 	}
 
 	return (
-		<div className="Login">
-			<Form onSubmit={handleSubmit}>
-				<Form.Group className="signin-layout" size="lg" controlId="name">
-					<Form.Label className="input-title">Name</Form.Label>
-					<Form.Control
-						className="input-block"
-						autoFocus
-						type="name"
-						onChange={(e) => setName(e.target.value)}
-					/>
-				</Form.Group>
-				<Form.Group className="signin-layout" size="lg" controlId="password">
-					<Form.Label className="input-title">Password</Form.Label>
-					<Form.Control
-						className="input-block"
-						type="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</Form.Group>
-				<div className="signin-buttons">
-					<Button block size="lg" disabled={!validateForm()} onClick={checkPassword}>
-						Submit
-					</Button>
-					<Button block size="lg" onClick={cancel}>
-						Cancel
-					</Button>
-				</div>
-			</Form>
+		<div>
+			<div className="Login">
+				<Form onSubmit={handleSubmit}>
+					<Form.Group className="signin-layout" size="lg" controlId="name">
+						<Form.Label className="input-title">Name</Form.Label>
+						<Form.Control
+							className="input-block"
+							autoFocus
+							type="name"
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</Form.Group>
+					<Form.Group className="signin-layout" size="lg" controlId="password">
+						<Form.Label className="input-title">Password</Form.Label>
+						<Form.Control
+							className="input-block"
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</Form.Group>
+					<div className="signin-buttons">
+						<Button block size="lg" disabled={!validateForm()} onClick={output}>
+							Submit
+						</Button>
+						<Button block size="lg" onClick={cancel}>
+							Cancel
+						</Button>
+					</div>
+				</Form>
+			</div>
 		</div>
 	);
 }
